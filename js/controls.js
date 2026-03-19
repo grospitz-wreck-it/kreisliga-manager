06eec0f1107f324df5c7e3862f181148fea9c195
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 15:27:35 +0100

    Update controls.js

--- js/controls.js
+++ js/controls.js
@@ -1,1 +1,24 @@
+function selectTeam() {
+  selectedTeam = document.getElementById("teamSelect").value;
+  updateTable();
+}
 
+function setTactic() {
+  selectedTactic = document.getElementById("tacticSelect").value;
+  document.getElementById("currentTactic").innerText =
+    "Taktik: " + selectedTactic;
+}
+
+function setLiveMode(mode) {
+  liveModifier = mode === "attack" ? 0.003 : -0.002;
+}
+
+function makeSub() {
+  if (substitutions <= 0) return alert("Keine Wechsel!");
+  substitutions--;
+  document.getElementById("subCount").innerText =
+    "Wechsel: " + substitutions;
+}
+
+function setSpeed(e, speed) {}
+function applyHalftime() {}
