2645443ed5cf2b4a5278f416d6269649dacc39e6
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 17:14:17 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -2,34 +2,33 @@ function updateTable() {
   let tbody = document.querySelector("#table tbody");
   tbody.innerHTML = "";
 
-  teams.sort((a,b)=>b.points-a.points);
+  teams
+    .sort((a,b)=>b.points-a.points)
+    .forEach(t=>{
+      let name = t.name === selectedTeam ? "👉 " + t.name : t.name;
 
-  teams.forEach(t=>{
-    let name = t.name === selectedTeam ? "👉 " + t.name : t.name;
-
-    tbody.innerHTML += `
-      <tr>
-        <td>${name}</td>
-        <td>${t.points}</td>
-        <td>${t.goals}</td>
-      </tr>
-    `;
-  });
+      tbody.innerHTML += `
+        <tr>
+          <td>${name}</td>
+          <td>${t.points}</td>
+          <td>${t.goals}</td>
+        </tr>
+      `;
+    });
 }
 
 function addEvent(text) {
   let box = document.getElementById("liveMatch");
-  let p = document.createElement("p");
-  p.textContent = text;
-  box.prepend(p);
+  box.innerHTML = `<p>${text}</p>` + box.innerHTML;
 }
+
 function updateScoreboard(t1, t2, s1, s2) {
-  document.getElementById("team1Name").innerText = t1.name;
-  document.getElementById("team2Name").innerText = t2.name;
-  document.getElementById("score").innerText = s1 + " : " + s2;
+  document.getElementById("score").innerText = `${s1} : ${s2}`;
+  document.getElementById("teamLeft").innerText = t1.name;
+  document.getElementById("teamRight").innerText = t2.name;
 }
 
 function updateTimeline(minute) {
-  let bar = document.getElementById("timelineBar");
-  bar.style.width = (minute / 90) * 100 + "%";
+  document.getElementById("timelineBar").style.width =
+    (minute / 90) * 100 + "%";
 }
