0cc6f582d52aff5b5eb5fed19c40fb9394348940
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 17:13:11 +0100

    Update league.js

--- js/league.js
+++ js/league.js
@@ -1,28 +1,20 @@
 let leagues = {
   "Kreisliga A Herford": [
-    { name: "TuS Bruchmühlen", strength: 75 },
-    { name: "VfL Holsen", strength: 72 },
-    { name: "Bünder SV", strength: 70 },
-    { name: "TSG Kirchlengern II", strength: 68 },
-    { name: "SV Löhne-Obernbeck", strength: 67 },
-    { name: "TuRa Löhne", strength: 66 },
-    { name: "GW Pödinghausen", strength: 65 },
-    { name: "SV Enger-Westerenger", strength: 64 },
-    { name: "TSV Löhne", strength: 63 },
-    { name: "SC Enger", strength: 62 },
-    { name: "FC Exter", strength: 61 },
-    { name: "SV Rödinghausen III", strength: 60 },
-    { name: "SV Oetinghausen", strength: 59 },
-    { name: "FA Herringhausen/Eickum", strength: 58 },
-    { name: "VfL Herford II", strength: 57 },
-    { name: "TuS Hücker-Aschen", strength: 56 }
+    { name: "TuS Bruchmühlen", strength: 70 },
+    { name: "VfL Holsen", strength: 68 },
+    { name: "Bünder SV", strength: 67 },
+    { name: "TSG Kirchlengern II", strength: 66 },
+    { name: "SV Löhne-Obernbeck", strength: 65 },
+    { name: "TuRa Löhne", strength: 64 },
+    { name: "GW Pödinghausen", strength: 63 },
+    { name: "SV Enger-Westerenger", strength: 62 },
+    { name: "TSV Löhne", strength: 61 },
+    { name: "SC Enger", strength: 60 },
+    { name: "FC Exter", strength: 59 },
+    { name: "SV Rödinghausen III", strength: 58 },
+    { name: "SV Oetinghausen", strength: 57 },
+    { name: "FA Herringhausen/Eickum", strength: 56 },
+    { name: "VfL Herford II", strength: 55 },
+    { name: "TuS Hücker-Aschen", strength: 54 }
   ]
 };
-
-function initLeague() {
-  teams = leagues["Kreisliga A Herford"].map(t => ({
-    ...t,
-    points: 0,
-    goals: 0
-  }));
-}
