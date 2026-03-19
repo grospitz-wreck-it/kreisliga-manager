e179f50b90bd35a85ce0f0ce52a5f2afdd54effa
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 15:26:50 +0100

    Update league.js

--- js/league.js
+++ js/league.js
@@ -1,1 +1,16 @@
+let leagues = {
+  "Kreisliga A Herford": [
+    { name: "TuS Bruchmühlen", strength: 75 },
+    { name: "VfL Holsen", strength: 70 },
+    { name: "Bünder SV", strength: 68 },
+    { name: "TSG Kirchlengern II", strength: 65 }
+  ]
+};
 
+function initLeague() {
+  teams = leagues["Kreisliga A Herford"].map(t => ({
+    ...t,
+    points: 0,
+    goals: 0
+  }));
+}
