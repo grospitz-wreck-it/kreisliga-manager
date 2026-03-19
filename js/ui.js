d03d8a3a9bf5ef7557c3806db36850733e7738ce
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 20:16:30 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -2,32 +2,37 @@ function updateTable() {
   let tbody = document.querySelector("#table tbody");
   tbody.innerHTML = "";
 
-  teams
-    .sort((a, b) => b.points - a.points)
-    .forEach(t => {
-      let name = t.name === selectedTeam ? "👉 " + t.name : t.name;
-
-      tbody.innerHTML += `
-        <tr>
-          <td>${name}</td>
-          <td>${t.points}</td>
-          <td>${t.goals}</td>
-        </tr>
-      `;
+  [...teams]
+    .sort((a,b)=> b.points-a.points || b.goals-a.goals)
+    .forEach(t=>{
+      let name = t.name === selectedTeam ? "👉 "+t.name : t.name;
+      tbody.innerHTML += `<tr><td>${name}</td><td>${t.points}</td><td>${t.goals}</td></tr>`;
     });
 }
-function addEvent(text) {
-  let box = document.getElementById("liveMatch");
-  box.innerHTML = `<p>${text}</p>` + box.innerHTML;
+
+function populateTeamSelect(){
+  const select=document.getElementById("teamSelect");
+  select.innerHTML="";
+  teams.forEach(t=>{
+    let o=document.createElement("option");
+    o.value=t.name;
+    o.textContent=t.name;
+    select.appendChild(o);
+  });
 }
 
-function updateScoreboard(t1, t2, s1, s2) {
-  document.getElementById("score").innerText = `${s1} : ${s2}`;
-  document.getElementById("teamLeft").innerText = t1.name;
-  document.getElementById("teamRight").innerText = t2.name;
+function addEvent(text){
+  let box=document.getElementById("liveMatch");
+  box.innerHTML=`<p>${text}</p>`+box.innerHTML;
 }
 
-function updateTimeline(minute) {
-  document.getElementById("timelineBar").style.width =
-    (minute / 90) * 100 + "%";
+function updateScoreboard(t1,t2,s1,s2){
+  document.getElementById("score").innerText=`${s1} : ${s2}`;
+  document.getElementById("teamLeft").innerText=t1.name;
+  document.getElementById("teamRight").innerText=t2.name;
 }
+
+function updateTimeline(minute){
+  document.getElementById("timelineBar").style.width=(minute/90)*100+"%";
+}
+
