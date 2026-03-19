69acd380106f970d41f43b82ef7bb6ebd5d227cf
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 18:54:45 +0100

    Update league.js

--- js/league.js
+++ js/league.js
@@ -1,8 +1,27 @@
-const leagues = {
-  "Kreisliga A Herford": [
-    "TuS Bruchmühlen","VfL Holsen","Bünder SV","TSG Kirchlengern II",
-    "SV Löhne-Obernbeck","TuRa Löhne","GW Pödinghausen","SV Enger-Westerenger",
-    "TSV Löhne","SC Enger","FC Exter","SV Rödinghausen III",
-    "SV Oetinghausen","FA Herringhausen/Eickum","VfL Herford II","TuS Hücker-Aschen"
-  ]
-};
+function loadLeague(name) {
+
+  if (name === "herford") {
+    teams = [
+      { name: "Bünder SV", points: 0, goals: 0 },
+      { name: "SC Herford", points: 0, goals: 0 },
+      { name: "TuS Bruchmühlen", points: 0, goals: 0 },
+      { name: "SV Rödinghausen II", points: 0, goals: 0 },
+      { name: "FC Exter", points: 0, goals: 0 },
+      { name: "SV Enger-Westerenger", points: 0, goals: 0 },
+      { name: "TuRa Löhne", points: 0, goals: 0 },
+      { name: "FC Herford", points: 0, goals: 0 },
+      { name: "TSV Löhne", points: 0, goals: 0 },
+      { name: "SV Oetinghausen", points: 0, goals: 0 },
+      { name: "SC Vlotho", points: 0, goals: 0 },
+      { name: "SV Bischofshagen", points: 0, goals: 0 },
+      { name: "TuS Hunnebrock", points: 0, goals: 0 },
+      { name: "SV Hiddenhausen", points: 0, goals: 0 },
+      { name: "FC Schweicheln", points: 0, goals: 0 },
+      { name: "RW Kirchlengern II", points: 0, goals: 0 }
+    ];
+  }
+
+  // 🔥 RESET (sehr wichtig)
+  currentMatchday = 0;
+  schedule = [];
+}
