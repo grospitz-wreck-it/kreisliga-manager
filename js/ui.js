bafbba004f4536a6ca816f3a3f1407f41036dc60
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 17:22:47 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -3,8 +3,8 @@ function updateTable() {
   tbody.innerHTML = "";
 
   teams
-    .sort((a,b)=>b.points-a.points)
-    .forEach(t=>{
+    .sort((a, b) => b.points - a.points)
+    .forEach(t => {
       let name = t.name === selectedTeam ? "👉 " + t.name : t.name;
 
       tbody.innerHTML += `
@@ -15,20 +15,4 @@ function updateTable() {
         </tr>
       `;
     });
-}
-
-function addEvent(text) {
-  let box = document.getElementById("liveMatch");
-  box.innerHTML = `<p>${text}</p>` + box.innerHTML;
-}
-
-function updateScoreboard(t1, t2, s1, s2) {
-  document.getElementById("score").innerText = `${s1} : ${s2}`;
-  document.getElementById("teamLeft").innerText = t1.name;
-  document.getElementById("teamRight").innerText = t2.name;
-}
-
-function updateTimeline(minute) {
-  document.getElementById("timelineBar").style.width =
-    (minute / 90) * 100 + "%";
 }
