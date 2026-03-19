601f85f164feb4be6f20f8eabfab9d33c53fb482
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 20:59:14 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-window.onload=function(){
-  let select=document.getElementById("leagueSelect");
+window.addEventListener("DOMContentLoaded", ()=>{
+
+  const leagueSelect = document.getElementById("leagueSelect");
+
   Object.keys(leagues).forEach(l=>{
-    let o=document.createElement("option");
-    o.value=l;
-    o.textContent=leagues[l];
-    select.appendChild(o);
+    const o = document.createElement("option");
+    o.value = l;
+    o.textContent = leagues[l];
+    leagueSelect.appendChild(o);
   });
-};
+
+  document.getElementById("btnStartLeague").onclick = selectLeague;
+  document.getElementById("btnSelectTeam").onclick = selectTeam;
+  document.getElementById("btnTactic").onclick = setTactic;
+  document.getElementById("startBtn").onclick = simulateMatchday;
+
+  document.getElementById("btnAttack").onclick = ()=>setLiveMode('attack');
+  document.getElementById("btnCalm").onclick = ()=>setLiveMode('calm');
+  document.getElementById("btnSub").onclick = makeSub;
+
+  document.querySelectorAll(".speed").forEach(b=>{
+    b.onclick = ()=> setSpeed(parseInt(b.dataset.speed));
+  });
+
+  document.getElementById("resumeBtn").onclick = resumeMatch;
+  document.getElementById("halfAttack").onclick = ()=>setLiveMode('attack');
+  document.getElementById("halfCalm").onclick = ()=>setLiveMode('calm');
+
+});
