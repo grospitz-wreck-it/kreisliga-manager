2faadedfc82fbca298dd1e811a9e2a15bca31195
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 15:27:22 +0100

    Update matchengine.js

--- js/matchengine.js
+++ js/matchengine.js
@@ -1,1 +1,20 @@
+function simulateMatchday() {
+  if (!selectedTeam) return alert("Team wählen!");
 
+  let t1 = teams[0];
+  let t2 = teams[1];
+
+  let s1 = Math.floor(Math.random()*3);
+  let s2 = Math.floor(Math.random()*3);
+
+  addEvent(`${t1.name} ${s1}:${s2} ${t2.name}`);
+
+  t1.goals += s1;
+  t2.goals += s2;
+
+  if (s1 > s2) t1.points += 3;
+  else if (s2 > s1) t2.points += 3;
+  else { t1.points++; t2.points++; }
+
+  updateTable();
+}
