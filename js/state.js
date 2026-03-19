4a991e99ce1cc2ecfcee45f8e1969314aec61f3d
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 20:38:04 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -7,7 +7,7 @@ let isSimulating = false;
 let currentInterval = null;
 let currentMinute = 0;
 
-let matchDuration = 180000;
+let speedMultiplier = 1; // 🔥 NEU echte Geschwindigkeit
 
 let liveModifier = 0;
 let substitutions = 5;
