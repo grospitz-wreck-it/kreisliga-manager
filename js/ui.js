c7f7f83b65bba0ebfdc81842619851aaa510121e
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 15:27:09 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,25 @@
+function updateTable() {
+  let tbody = document.querySelector("#table tbody");
+  tbody.innerHTML = "";
 
+  teams.sort((a,b)=>b.points-a.points);
+
+  teams.forEach(t=>{
+    let name = t.name === selectedTeam ? "👉 " + t.name : t.name;
+
+    tbody.innerHTML += `
+      <tr>
+        <td>${name}</td>
+        <td>${t.points}</td>
+        <td>${t.goals}</td>
+      </tr>
+    `;
+  });
+}
+
+function addEvent(text) {
+  let box = document.getElementById("liveMatch");
+  let p = document.createElement("p");
+  p.textContent = text;
+  box.prepend(p);
+}
