17ee679966dcc0d88c7b87e017fcf78f68f039a3
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 20:17:35 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-window.onload = function () {
-
-  let leagueSelect = document.getElementById("leagueSelect");
-
-  Object.keys(leagues).forEach(l => {
-    let o = document.createElement("option");
-    o.value = l;
-    o.textContent = l;
-    leagueSelect.appendChild(o);
+window.onload=function(){
+  let select=document.getElementById("leagueSelect");
+  Object.keys(leagues).forEach(l=>{
+    let o=document.createElement("option");
+    o.value=l;
+    o.textContent=leagues[l];
+    select.appendChild(o);
   });
-
-  document.getElementById("matchday").innerText =
-    "Spieltag: 0 / 0";
 };
