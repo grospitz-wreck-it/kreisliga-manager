8feab7fa1250a6e5c9e14a6a7bcf97a3f25ae992
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 17:22:27 +0100

    Update league.js

--- js/league.js
+++ js/league.js
@@ -1,20 +1,8 @@
-let leagues = {
+const leagues = {
   "Kreisliga A Herford": [
-    { name: "TuS Bruchmühlen", strength: 70 },
-    { name: "VfL Holsen", strength: 68 },
-    { name: "Bünder SV", strength: 67 },
-    { name: "TSG Kirchlengern II", strength: 66 },
-    { name: "SV Löhne-Obernbeck", strength: 65 },
-    { name: "TuRa Löhne", strength: 64 },
-    { name: "GW Pödinghausen", strength: 63 },
-    { name: "SV Enger-Westerenger", strength: 62 },
-    { name: "TSV Löhne", strength: 61 },
-    { name: "SC Enger", strength: 60 },
-    { name: "FC Exter", strength: 59 },
-    { name: "SV Rödinghausen III", strength: 58 },
-    { name: "SV Oetinghausen", strength: 57 },
-    { name: "FA Herringhausen/Eickum", strength: 56 },
-    { name: "VfL Herford II", strength: 55 },
-    { name: "TuS Hücker-Aschen", strength: 54 }
+    "TuS Bruchmühlen","VfL Holsen","Bünder SV","TSG Kirchlengern II",
+    "SV Löhne-Obernbeck","TuRa Löhne","GW Pödinghausen","SV Enger-Westerenger",
+    "TSV Löhne","SC Enger","FC Exter","SV Rödinghausen III",
+    "SV Oetinghausen","FA Herringhausen/Eickum","VfL Herford II","TuS Hücker-Aschen"
   ]
 };
