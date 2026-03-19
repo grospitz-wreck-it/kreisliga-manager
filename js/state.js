f69b42c2235f76d6aab45dfe799785ebf6321256
grospitz-wreck-it <grospitz@gmail.com>
2026-03-19 17:12:56 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,10 +1,16 @@
-let schedule = [];
 let teams = [];
+let schedule = [];
 let currentMatchday = 0;
 let selectedTeam = null;
 let selectedTactic = "normal";
-let selectedFormation = "4-4-2";
 
 let isSimulating = false;
-let substitutions = 5;
+let currentInterval = null;
+
+let matchStartTime = 0;
+let matchDuration = 180000; // 3 Minuten
+
+let halftimeDone = false;
+
 let liveModifier = 0;
+let substitutions = 5;
