99b733171e1e04e6a4c0ae56e7293d7165b8e898
grospitz-wreck-it <grospitz@gmail.com>
2026-03-20 06:55:57 +0100

    Update matchEngine.js

--- js/matchEngine.js
+++ js/matchEngine.js
@@ -1,21 +1,130 @@
 function generateSchedule(){
-    } else if(r<0.10){
-      addEvent(`🟨 ${currentMinute}' Gelb`);
-    } else if(r<0.15){
-      addEvent(`💥 Chance`);
+  schedule = [];
+  let temp = [...teams];
+  let n = temp.length;
+
+  for(let r=0; r<n-1; r++){
+    let day = [];
+    for(let i=0; i<n/2; i++){
+      day.push([temp[i], temp[n-1-i]]);
+    }
+    schedule.push(day);
+
+    let last = temp.pop();
+    temp.splice(1, 0, last);
+  }
+
+  let second = schedule.map(d => d.map(m => [m[1], m[0]]));
+  schedule = schedule.concat(second);
+}
+
+
+function simulateMatchday(){
+  if(isSimulating) return;
+  if(!selectedTeam){
+    alert("Team wählen!");
+    return;
+  }
+
+  lockTeam();
+
+  let matches = schedule[currentMatchday];
+  let userMatch = matches.find(m =>
+    m[0].name === selectedTeam || m[1].name === selectedTeam
+  );
+
+  matches.forEach(m => {
+    if(m !== userMatch){
+      simulateQuick(m[0], m[1]);
+    }
+  });
+
+  simulateLiveMatch(userMatch[0], userMatch[1]);
+}
+
+
+function simulateQuick(t1, t2){
+  let s1 = Math.floor(Math.random()*3);
+  let s2 = Math.floor(Math.random()*3);
+
+  t1.goals += s1;
+  t2.goals += s2;
+
+  if(s1 > s2) t1.points += 3;
+  else if(s2 > s1) t2.points += 3;
+  else {
+    t1.points++;
+    t2.points++;
+  }
+}
+
+
+function simulateLiveMatch(t1, t2){
+  if(currentInterval) clearInterval(currentInterval);
+
+  isSimulating = true;
+  substitutions = 5;
+  liveModifier = 0;
+  currentMinute = 0;
+
+  document.getElementById("liveMatch").innerHTML = "";
+
+  liveScore = { t1, t2, s1: 0, s2: 0 };
+
+  updateScoreboard(t1, t2, 0, 0);
+
+  startInterval();
+}
+
+
+function startInterval(){
+  clearInterval(currentInterval);
+
+  let intervalTime = 1000 / speedMultiplier;
+
+  currentInterval = setInterval(() => {
+
+    currentMinute++;
+
+    let rand = Math.random();
+
+    if(rand < 0.05 + liveModifier){
+      if(Math.random() < 0.5){
+        liveScore.s1++;
+        addEvent("⚽ " + currentMinute + "' " + liveScore.t1.name);
+      } else {
+        liveScore.s2++;
+        addEvent("⚽ " + currentMinute + "' " + liveScore.t2.name);
+      }
+    }
+    else if(rand < 0.10){
+      addEvent("🟨 " + currentMinute + "' Gelbe Karte");
+    }
+    else if(rand < 0.15){
+      addEvent("💥 " + currentMinute + "' Große Chance");
+    }
+    else if(rand < 0.20){
+      addEvent("🧤 " + currentMinute + "' Parade");
     }
 
-    updateScoreboard(liveScore.t1,liveScore.t2,liveScore.s1,liveScore.s2);
+    updateScoreboard(
+      liveScore.t1,
+      liveScore.t2,
+      liveScore.s1,
+      liveScore.s2
+    );
+
     updateTimeline(currentMinute);
 
-    if(currentMinute===45){
+    if(currentMinute === 45){
       clearInterval(currentInterval);
-      isSimulating=false;
-      document.getElementById("halftimePanel").style.display="block";
+      isSimulating = false;
+      document.getElementById("halftimePanel").style.display = "block";
+      addEvent("⏸️ Halbzeit");
       return;
     }
 
-    if(currentMinute>=90){
+    if(currentMinute >= 90){
       clearInterval(currentInterval);
       finishMatch();
     }
@@ -23,31 +132,41 @@ function generateSchedule(){
   }, intervalTime);
 }
 
+
 function restartInterval(){
   startInterval();
 }
 
+
 function resumeMatch(){
-  document.getElementById("halftimePanel").style.display="none";
-  isSimulating=true;
+  document.getElementById("halftimePanel").style.display = "none";
+  isSimulating = true;
   startInterval();
 }
 
+
 function finishMatch(){
-  isSimulating=false;
+  isSimulating = false;
 
-  let {t1,t2,s1,s2}=liveScore;
+  let { t1, t2, s1, s2 } = liveScore;
 
-  t1.goals+=s1; t2.goals+=s2;
+  addEvent("🏁 Endstand: " + s1 + ":" + s2);
 
-  if(s1>s2) t1.points+=3;
-  else if(s2>s1) t2.points+=3;
-  else { t1.points++; t2.points++; }
+  t1.goals += s1;
+  t2.goals += s2;
+
+  if(s1 > s2) t1.points += 3;
+  else if(s2 > s1) t2.points += 3;
+  else {
+    t1.points++;
+    t2.points++;
+  }
 
   currentMatchday++;
 
-  document.getElementById("matchday").innerText=
-    "Spieltag: "+currentMatchday+" / "+schedule.length;
+  document.getElementById("matchday").innerText =
+    "Spieltag: " + currentMatchday + " / " + schedule.length;
 
   updateTable();
+  document.getElementById("startBtn").disabled = false;
 }
