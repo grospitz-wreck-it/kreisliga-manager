7dc4b5e0b25ddabb2d70900bb0460ba518217fdc
grospitz-wreck-it <grospitz@gmail.com>
2026-03-23 18:21:43 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,4 @@
 const SUPABASE_URL = "https://kckwxggzoenybssryaqr.supabase.co";
 const SUPABASE_KEY = "sb_publishable_SJS03utwmfMRDO2kISTv3A_vIIk6DsSY";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
