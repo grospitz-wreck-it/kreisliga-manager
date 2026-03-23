32a5eee035a7616c47b144bba34c11157a52c087
grospitz-wreck-it <grospitz@gmail.com>
2026-03-23 18:44:14 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -5,7 +5,7 @@
 if(!window.supabaseClient){
 
   const SUPABASE_URL = "https://kckwxggzoenybssryaqr.supabase.co";
-  const SUPABASE_KEY = "sb_publishable_SJS03utwmfMRDO2kISTv3A_vIIk6DsSYY";
+  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtja3d4Z2d6b2VueWJzc3J5YXFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyODA1NTksImV4cCI6MjA4OTg1NjU1OX0.J6zOyaBcrXphox1zwLn-bUOYP6SrWxs3_1x4z8B6ZDE";
 
   window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
