daae4b262ec4d9c49d4af0ad7aba55ceb8dca69b
grospitz-wreck-it <grospitz@gmail.com>
2026-03-28 11:36:35 +0100

    Update bindings.js

--- js/ui/bindings.js
+++ js/ui/bindings.js
@@ -1,7 +1,7 @@
 function bindUI(){
 
   document.getElementById("mainButton").onclick = handleMainAction;
-
+  document.getElementById("teamSelect").onchange = selectTeam;
   document.querySelectorAll(".speedControl button").forEach(b=>{
     b.onclick = ()=> setSpeed(parseInt(b.innerText));
   });
