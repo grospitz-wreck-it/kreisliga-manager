cbc30f4ed967b5059fb774f0ca97570ae34e9d42
grospitz-wreck-it <grospitz@gmail.com>
2026-03-28 18:59:37 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 window.onload = async () => {
 
+  // UI initialisieren
   initLeagueSelect();
+  bindUI();
 
+  // 🔥 ERSTE LIGA AUTOMATISCH LADEN
+  const firstLeague = Object.keys(LEAGUES)[0];
+
+  if(firstLeague){
+    document.getElementById("leagueSelect").value = firstLeague;
+    selectLeague(firstLeague);
+  }
+
+  // Save laden
   const loaded = await loadGame();
 
   if(loaded){
