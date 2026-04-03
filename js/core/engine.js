a678b885f95eec4852ab647ae90b428ee01ab12a
grospitz-wreck-it <grospitz@gmail.com>
2026-04-03 16:46:35 +0200

    Update engine.js

--- js/core/engine.js
+++ js/core/engine.js
@@ -2,6 +2,7 @@
 // 🌍 MATCH STATE
 // =========================
 import { renderSchedule } from "../ui/ui.js";
+import { renderTable, renderLiveTable } from "../ui/table.js";
 import { updateUI, renderLiveFeed } from "../ui/ui.js";
 import { emit } from "./events.js";
 import { EVENTS } from "./events.constants.js";
