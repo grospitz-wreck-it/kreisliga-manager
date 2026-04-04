86df6c4916e549d8092f0888868ced87cdaf7708
grospitz-wreck-it <grospitz@gmail.com>
2026-04-04 16:41:14 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
-import { SUPABASE_URL, SUPABASE_KEY } from "../config.js";
+import { SUPABASE_URL, SUPABASE_KEY } from "./config.js";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
