cceb1da7a233087373a3c789a2cc7762d58b2ec6
grospitz-wreck-it <grospitz@gmail.com>
2026-04-04 16:07:03 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // ⚙️ GLOBAL CONFIG
 // =========================
 
-export const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-export const SUPABASE_KEY = "YOUR_PUBLIC_ANON_KEY";
+export const SUPABASE_URL = "https://kckwxggzoenybssryaqr.supabase.co";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtja3d4Z2d6b2VueWJzc3J5YXFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyODA1NTksImV4cCI6MjA4OTg1NjU1OX0.J6zOyaBcrXphox1zwLn-bUOYP6SrWxs3_1x4z8B6ZDE";
