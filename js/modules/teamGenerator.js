d16d192fef9cf4e15ace32dfd0d305e4f35417c5
grospitz-wreck-it <grospitz@gmail.com>
2026-04-04 19:15:36 +0200

    Update teamGenerator.js

--- js/modules/teamGenerator.js
+++ js/modules/teamGenerator.js
@@ -1,17 +1,31 @@
-export function extractTeams(data) {
-  const teams = [];
+export function extractLeagues(data) {
+  const leagues = [];
 
   data.forEach(row => {
+
+    const league = {
+      name: row["Liga"],
+      kreis: row["Kreis"],
+      bundesland: row["Bundesland"],
+      teams: []
+    };
+
     Object.keys(row).forEach(key => {
       if (key.startsWith("Team") && row[key]) {
-        teams.push({
+        league.teams.push({
           name: row[key],
           liga: row["Liga"],
           kreis: row["Kreis"]
         });
       }
     });
+
+    // 👉 nur hinzufügen wenn Teams existieren
+    if (league.teams.length > 0) {
+      leagues.push(league);
+    }
+
   });
 
-  return teams;
+  return leagues;
 }
